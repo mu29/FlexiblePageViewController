1fe078223baf0e7a3071e250f891a6f462f5d06b
InJung Chung <mu29@yeoubi.net>
2017-04-14 13:51:24 +0900

    Remove Pods folder

--- Example/Pods/Target Support Files/FlexiblePageViewController/FlexiblePageViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FlexiblePageViewController : NSObject
-@end
-@implementation PodsDummy_FlexiblePageViewController
-@end
--- Example/Pods/Target Support Files/Pods-FlexiblePageViewController_Example/Pods-FlexiblePageViewController_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlexiblePageViewController_Example : NSObject
-@end
-@implementation PodsDummy_Pods_FlexiblePageViewController_Example
-@end
